7effe261f271d840a718d6bbb58c8bcb8041bcb6
VedantBandre <vedantbandre33@gmail.com>
2024-02-07 23:21:07 +0100

    small changes

--- django_website/blog/views.py
+++ django_website/blog/views.py
@@ -1,9 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+
+# Fake Posts
+
+posts = [
+	{
+		'author': 'auth1',
+		'title': 'Blog Post 1',
+		'content': 'First post conent',
+		'date_posted': 'February 7, 2024'
+	},
+	{
+		'author': 'auth2',
+		'title': 'Blog Post 2',
+		'content': 'Second post conent',
+		'date_posted': 'February 8, 2024'
+	},
+]
+
+
 
 # Create your views here.
 def home(request):
-	return HttpResponse('<h1>Blog Home</h1>')
+	context = { 
+		'posts': posts
+	}
+	return render(request, 'blog/home.html', context)
 
 def about(request):
-	return HttpResponse('<h1>Blog About</h1>')
+	return render(request, 'blog/about.html', {'title': 'About'})
